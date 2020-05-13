cd1cbe4d8dbf00774ef3bd2e5a2048d6d338e370
Dwight Nodolf <dnodolf@gmail.com>
2020-05-13 20:54:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,8 @@ class Song
   def self.new_by_filename(filename)
     array = filename.chomp(".mp3").split(" - ")
     song = self.new(array[1])
-    
+    song.artist = array[0]
+    song
   end 
     
   
