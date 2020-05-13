56d198561c5d843c0be4f69cf88cb33cdd1fce3c
Dwight Nodolf <dnodolf@gmail.com>
2020-05-13 21:01:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,7 @@ class Song
   
   def artist= (artist)
     @artist = artist
-    artist.add_song(self) unless artist.songs.include?(self)
+    #artist.add_song(self) unless artist.songs.include?(self)
   end 
   
   def self.new_by_filename(filename)
