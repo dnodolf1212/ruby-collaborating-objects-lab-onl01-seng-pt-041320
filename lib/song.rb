ef3b3e60d6a3c818a1975a467188865fcc6cf868
Dwight Nodolf <dnodolf@gmail.com>
2020-05-13 21:17:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,12 +22,15 @@ class Song
   def artist= (artist)
     @artist = artist
     artist.add_song(self) #unless artist.songs.include?(self)
-  end 
+  end
+  
+  def artist_name=(name)
+    
   
   def self.new_by_filename(filename)
     array = filename.chomp(".mp3").split(" - ")
     song = self.new(array[1])
-    song.artist = array[0]
+    song.artist_name = array[0]
     
   end 
     
